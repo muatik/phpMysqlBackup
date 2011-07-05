4dfb54fd4a70c31b693df0c9f0afd8243e349b89
muatik <muatik@gmail.com>
2011-07-05 16:02:50 +0300

    İlk yükleme.

---
+++ mailman.php
@@ -0,0 +1,164 @@
+<?
+/*
+MAILMAN 0.1
+---------------------------
+Tarih: 14 Ağustos(08) 2006
+Yazan: Mustafa Atik
+E-Posta: mr.mustafaatik@gmail.com
+Detaylı Bilgi: http://cookingthecode.com/
+
+Bu uygulama, php'nin mail() fonsiyonunu kullanmadan, smtp sunucusuna soket bağlantısı kurarak e-posta mesajı gönderir.
+En önemli özelliği, e-posta mesajına iliştirilmiş dosyalar gönderilebilmesine imkan vermesidir.
+
+Bu uygulamayı istediğiniz şekilde kullanabilirsiniz. İyi kullanın.
+
+#DIKKAT#
+Bu uygulama henüz tamamlanmamıştır fakat aciliyet nedeniyle bu haliyle kullanılmaktadır. 
+Düzeltilmesi gereken bazı kısımları vardır.
+1- boundary random türetilecek.
+2- send kısmında response lar alınmadan denennecek
+3- @fclose($fs); olmayacak
+4- '@' ile hataların kontrolünden vazgeçmelidir.
+5- kısayol metodu yazılmalıdır örn:   qSend($from,$to,$subject,$msg,$extHedars,$files) gibi.
+
+*/
+require_once('mimeType.php');
+class mailman
+{
+	var $host='127.0.0.1';
+	var $port='25';
+	var $headers;
+	var $from;
+	var $from_full_name='';
+	var $to;
+	var $content_type='text/plain; charset=utf-8; format=flowed';
+	var $subject;
+	var $content;
+	var $boundary;
+	var $files;
+	var $error_code;
+	var $error;
+	var $user_headers='';
+	var $body=null;
+	
+	function error_throw($error_code,$ext_str='')
+	{
+		$this->error_code=$error_code;
+		switch($error_code)
+		{
+			case 1:$this->error='SMTP sunucusuna bağlanılamadı.'.$ext_str;break;
+			case 3:$this->error='SMTP sunucusuna veri gönderilirken hata oluştu.'.$ext_str;break;
+			case 2:$this->error='SMTP sunucusundan veri okunurken hata oluştu.'.$ext_str;break;
+			case 10:$this->error=$ext_str.' isimli dosya açılamıyor. ';break;
+			case 11:$this->error=$ext_str.' isimli dosya okunamıyor. ';break;
+			default:$this->error='Belirsiz bir hata oluştu. '.$ext_str;break;
+		}
+		return true;
+	}
+	
+	function file_read($file_name)
+	{
+		if($fp=@fopen($file_name,'r'))
+		{
+			if($data=@fread($fp,filesize($file_name))) return $data;
+			$this->error_throw(11,$file_name);
+		}else $this->error_throw(10,$file_name);
+		return false;
+	}
+	
+	function prepare()
+	{
+		if(($result=$this->prepare_headers())===true)	return prepare_body();
+	}
+	
+	function prepare_headers($call=true)
+	{
+		if($this->from_full_name=='') $this->from_full_name=$this->from;
+		$this->headers="From: \"".$this->from_full_name."\" <".$this->from.">\r\n";
+		$this->headers.="To: ".$this->to."\r\n";
+		$this->headers.="Date: ".date('D, d M Y H:i:s')."\r\n";
+		$this->headers.="Subject: ".$this->subject."\r\n";
+		$this->headers.="Reply-To:  ".$this->from."\r\n";
+		$this->headers.="Return-Path: ".$this->from."\r\n";
+		if(trim($this->user_headers)!='') $this->headers.=$this->user_headers."\r\n";
+		if(count($this->files)>0)
+		{
+			$this->boundary="--bndry0123";
+			$this->headers.="MIME-Version: 1.0\r\n";
+			$this->headers.="Content-Type: multipart/mixed; boundary=".$this->boundary."\r\n";
+			$this->headers.="\r\nThis is multi-part message\r\n";
+			if($call) $this->prepare_body();
+		}
+		else
+		{
+			$this->headers.="Content-Type: ".$this->content_type."\r\n";
+			$this->headers.="Content-Transfer-Encoding: 8bit\r\n";
+		}
+		$this->headers.="\r\n";
+		return true;
+	}
+	
+	function prepare_body()
+	{
+		if($this->headers=='') $this->prepare_headers(false);
+		if(count($this->files)>0) //attachment mail
+		{
+			$this->body="--".$this->boundary."\r\n";
+			$this->body.="Content-type: ".$this->content_type."\r\n";
+			$this->body.="Content-transfer-encoding: 7bit\r\n\r\n";
+			$this->body.=$this->content."\r\n\r\n";
+			foreach($this->files as $file)
+			{
+				if(($data=$this->file_read($file))!==false)
+				{
+					//$mimeType=mime_content_type($file) // ilgili kütüpane yüklü olmadığından etkisiz hale getirildi.
+					$mimeType=getMimeType($file); // üsteki satır yerine benim yazdığım bir fonksiyon.
+					$file=explode('/',$file);$file=$file[count($file)-1];
+					$this->body.="--".$this->boundary."\r\n";
+					$this->body.="Content-type: ".$mimeType.";name=\"".$file."\"\r\n";
+					$this->body.="Content-transfer-encoding: base64\r\n";
+					$this->body.="Content-disposition: attachment;filename=\"".$file."\"\r\n\r\n";
+					$this->body.=chunk_split(base64_encode($data));
+				}
+			}
+		}
+		else //simple text mail
+		{
+			$this->body=$this->content;
+		}
+		$this->body.="\r\n.\r\n";
+		return true;
+	}
+	function send()
+	{
+		if($this->headers=='' || $this->body=='') $this->prepare_body();
+		if(!$fs=fsockopen($this->host,$this->port)) {$this->error_throw(1); return false;}
+		if(!$response=@fgets($fs,128)) {@fclose($fs);$this->error_throw(3);return false;}
+		if(!@fwrite($fs,"HELO ".$this->host."\n")) {@fclose($fs);$this->error_throw(2);return false;}
+		if(!$response=@fgets($fs,128)) {@fclose($fs);$this->error_throw(3);return false;}
+		if(!@fwrite($fs,"MAIL From: ".$this->from."\n")) {@fclose($fs);$this->error_throw(2);return false;}
+		if(!$response=@fgets($fs,128)) {@fclose($fs);$this->error_throw(3);return false;}
+		if(!@fwrite($fs,"RCPT To: ".$this->to."\n")) {@fclose($fs);$this->error_throw(2);return false;}
+		if(!$response=@fgets($fs,128)) {@fclose($fs);$this->error_throw(3);return false;}
+		if(!@fwrite($fs,"DATA\n")) {@fclose($fs);$this->error_throw(2);return false;}
+		if(!$response=@fgets($fs,128)) {@fclose($fs);$this->error_throw(3);return false;}
+		if(!@fwrite($fs,$this->headers.$this->body)){@fclose($fs);$this->error_throw(2);return false;}
+		if(!$response=@fgets($fs,128)) {@fclose($fs);$this->error_throw(3);return false;}
+		fclose($fs);
+		if(substr($response,0,3)==250) return true;
+		return $response;
+	}
+}
+
+function send_email($to,$subject,$content,$from='bilgi@Kocaelide.com',$full_name='Kocaelide.com')
+{
+	$p=new mailman();
+	$p->content_type='text/plain; charset=iso-8859-9; format=flowed';
+	$p->from=mb_convert_encoding($from,'ISO-8859-9','UTF-8');
+	$p->from_full_name=mb_convert_encoding($full_name,'ISO-8859-9','UTF-8');
+	$p->to=mb_convert_encoding($to,'ISO-8859-9','UTF-8');
+	$p->subject=mb_convert_encoding($subject,'ISO-8859-9','UTF-8');
+	$p->content=mb_convert_encoding($content,'ISO-8859-9','UTF-8');
+	return $p->send();
+}
+?>
---
+++ phpMysqlBackup.php
@@ -0,0 +1,211 @@
+<?php
+/*
+PHP MYSQL BACKUP 0.5
+---------------------------
+Tarih: 13 Kasım(11) 2008
+Yazan: Mustafa Atik
+E-Posta: mr.atik@gmail.com
+Detaylı Bilgi: http://cookingthecode.com
+
+Bu uygulama, mysql veritabanı sunucularıyla çalışabilecek bir yedekleme uygulamasıdır.
+İstenildiği veya önceden belirlenen zamanlarda; istenilen veritabanlarını, tabloları ve tablolardaki kayıtları tekrar 
+oluşturabilecek sql kodu; bir ftp adresine, bir e-posta kutusuna gönderilebilir veya yerel dizinlere kaydedilebilir.
+
+Bu uygulamayı istediğiniz şekilde kullanabilirsiniz. İyi kullanın.
+*/
+ini_set('mbstring.internal_encoding','UTF-8');
+error_reporting(E_ALL);
+require_once('db_connection.php');	// veritabanı nesnesinin yaratıldığı sınıf
+require_once('mailman.php');		// dosya iliştirilmiş e-posta mesajlarının gönderilmesine imkan sunan sınıf. Biraz eski sayılabilir.
+class phpMysqlBackup
+{
+	public $db;								// veritabanı işlemlerinin yapılacağı nesne (db_connection.php)
+	public $schedules=array();				// zamanlanmış iş listesi
+	public $logFile='tmp/phpMysqlBackup.txt';	// zamanlanmış işlerin en son ne zaman çalıştırıldığı bu dosyada okunacak. eğer boş ise, görevler her tetiklemede çalıştırılır.
+	private $oldLogs=array();					// kod tarafından kullanılan yerel değişkendir. hata durumunda log dosyasından okunan değerleri değişlik olamdan geri yazılmasını sağlar.  
+	public $logs=array();						// $logFile dosyasından okunana çalışma zamanları, bu dizi değişkende tutulur. 
+	public $ddlDB='information_schema';		// veritabanını sunucusunda bulunan neslelerle(table, column, index) ilgili bilgilerin saklandığı veritabanıdır. Mysql'de bu bilgileri information_schema veritabanı tutar.
+	public $tmpDir='tmp/';					// oluşturulan sql ifadeleri, ftp veya eposta ile gönderilmeden önce geçici olarak bir dosyaya yazılmalıdır. bu değişlen, dosyanın dizinini belirtir.
+	
+	public function __construct()
+	{
+		// örnek görev tanımlamaları için: http://cookingthecode.com/
+		$this->schedules=array(
+			'2880'=>array
+			(
+			'db'=>array('host'=>'localhost','user'=>'mysql_kullanici','password'=>'mysql_parola'),
+			'dbObjects'=>array('siteDB'=>array('*')),
+			'email'=>array('to'=>'mr.atik@gmail.com','title'=>'CookinTC DB Yedeği - ','from'=>'bilgi@cookingthecode.com','fileName'=>'cookingVtYedek','timeSuffix'=>true,'titleTimeSuffix'=>true,'message'=>'Cookingthecode.com Veritabanı Yedeğidir. Lak lak lak...'),
+			'local'=>array('fileName'=>'ctcDB','timeSuffix'=>true),
+			'ftp'=>array('address'=>'site.com','username'=>'ftp_user','password'=>'ftp_password','remoteDir'=>'/public_html/','fileName'=>'aaYedek','timeSuffix'=>true)
+			)
+		);
+	}
+	function readLogs()	// önceden kaydedilmiþ iþlerin, yapýlýþ zamanlarý dosyadan okunacak
+	{
+		$this->logs=array();
+		if($this->logFile==null) {$this->logs=array(); return true;}
+		if(file_exists($this->logFile)) $f=file_get_contents($this->logFile); else $f='';
+		if($f=='lock') {$this->error='Şu an başka bir yedekleme işlemi yapılmaka olduğu için, bu yedekleme işlemi iptal edildi.' ;return false;}
+		file_put_contents($this->logFile,'lock');	// diğer işlemlerin, şu an yapılan yedeklemenin farkında olmalarını sağlayacak.
+		
+		if($f=='') return true;
+		$f=explode("\n",$f);
+		foreach($f as $i)
+		{
+			if($i=='') continue;
+			$i=explode('=',$i);
+			$this->logs[$i[0]]=$i[1];
+		}
+		$this->oldLogs=$this->logs;
+		return true;
+	}
+	function writeLogs()	// $logs dizisindeki veriler dosyaya kaydedilecek
+	{
+		if($this->logFile==null) $this->logs=array();
+		$fContent='';
+		foreach($this->logs as $k=>$i)
+		{
+			$fContent.=$k.'='.$i."\n";
+		}
+		return file_put_contents($this->logFile,$fContent,LOCK_EX);
+	}
+	function makeSchedules()	// zamanlanmýþ görev listesindeki her bir iþ parçacýðý iþlenecek
+	{
+		if(!$this->readLogs()) return false;
+		
+		foreach($this->schedules as $t=>$s)
+		{
+			//iş parçacığı daha önceden çalıştırılmış ve belirtilen süre kadar vakit henüz geçmemişse işi yapma
+			if(isset($this->logs[$t]) && $t>((time()-$this->logs[$t])/60)) continue;
+			if($this->db==null) $this->db=new dbConnection();
+			
+			// veritabanı sunucususuna bağlanırken kullanılacak bilgileri atanıyor
+			$this->db->host=$s['db']['host'];
+			$this->db->password=$s['db']['password'];
+			$this->db->username=$s['db']['user'];
+			
+			$sql="/*PhpMysqlBackup\n".date('Y-m-d h:i:s')."*/\nSET FOREIGN_KEY_CHECKS=0;\nSET UNIQUE_CHECKS=0;\n";
+			
+			 foreach($s['dbObjects'] as $dbName=>$tbls)
+			 {
+			 	// belirtilen veritabanı için bağlantı açılıyor.
+			 	$this->db->database=$dbName;
+			 	if($this->db->connect()===false) {$this->writeLogs();return false;}
+			 	$sql.="use ".$dbName.";\n\n";
+			 	
+			 	// veritabanındaki tüm tablolar belirtilmişse, tablo listesi alınıyor.
+			 	$tList=array();
+			 	if((is_array($tbls) && $tbls[0]=='*') || $tbls=='*') $tList=$this->fetchTables($dbName); 
+			 	
+			 	// belirtilen göz ardı tablolar, tablo listesinden çıkartılıyor ve işlem yapılacak tablolar, tablo listesine aktarılıyor.
+			 	if(is_array($tbls))
+			 	foreach($tbls as $i)
+			 	{
+				 	if($i=='*') continue; 
+				 	elseif(mb_substr($i,0,2)=='--')
+				 	{
+				 		foreach($tList as $j=>$k) if($k->table_name==mb_substr($i,2)) unset($tList[$j]);
+				 	}
+				 	else
+				 	{
+				 		$tObj->table_name=$i;
+				 		$tList[]=$tObj;
+				 	} 
+			 	}
+			 	
+			 	// yedeklenecek tablolar birer birer işleniyor
+			 	foreach($tList as $i)
+			 	{
+			 		// tabloyu oluşturacak kod çekiliyor.
+			 		$crtT=$this->db->fetchListByQuery('show create table '.$i->table_name,'array');
+			 		$sql.=$crtT[0]['Create Table'].";\n\n";
+			 		
+			 		// tablodaki veriler çekiliyor
+			 		$records=$this->db->fetchListByQuery('select * from '.$i->table_name,'row');
+			 		if(count($records)>0)
+			 		{
+			 			$sql.='insert into '.$i->table_name." values ";
+			 			foreach($records as $r)
+			 			{
+			 				foreach($r as $rCK=>$rC) $r[$rCK]=addslashes($rC);
+			 				$sql.="\n('".implode('\',\'',$r)."'),";
+			 			}
+			 			$sql=mb_substr($sql,0,-1).";\n";
+			 		}
+			 		
+			 		$sql.="\n";
+			 	}
+			 }
+			 
+			 $sql.="\nSET FOREIGN_KEY_CHECKS=0;\nSET UNIQUE_CHECKS=1;";
+			 $this->logs[$t]=time();
+			 
+			 if(isset($s['local'])) $this->sendToFile($s,$sql);
+			 if(isset($s['ftp'])) $this->sendToFtp($s,$sql);
+			 if(isset($s['email'])) $this->sendToEmail($s,$sql);
+		}
+		
+		$this->oldLogs=$this->logs;
+		$this->writeLogs();
+	}
+	private function fetchTables()
+	{
+		$tList=$this->db->fetchListByQuery('select table_name,table_type,engine,auto_increment,update_time,table_collation,collations.character_set_name,table_comment from '.$this->ddlDB.'.tables ,'.$this->ddlDB.'.collations where tables.table_collation=collations.collation_name and table_schema=\''.$this->db->database.'\'');
+		if($tList==false) return false;
+		return $tList;
+	}
+	private function sendToFile($s,$sql)
+	{
+		$fname=$s['local']['fileName'];
+		if(isset($s['local']['timeSuffix']) && $s['local']['timeSuffix']==true) $fname.=date('Y_m_d_H_i');
+		$fname.='.sql';
+		return file_put_contents($fname,$sql);
+	}
+	private function sendToEmail($s,$sql)
+	{
+		$eParams=$s['email'];
+		$fname=$eParams['fileName'];
+		if(isset($fParams['timeSuffix']) && $fParams['timeSuffix']==true) $fname.=date('Y_m_d_H_i');
+		$fname.='.sql';
+		file_put_contents($this->tmpDir.$fname,$sql);
+		
+		$mm=new mailman();
+		$mm->from=$eParams['from'];
+		$mm->subject=$eParams['title'].(isset($eParams['titleTimeSuffix']) && $eParams['titleTimeSuffix']?date('Y-m-d H:i'):'');
+		if(isset($eParams['message'])) $mm->content=$eParams['message'];
+		$mm->files=array($this->tmpDir.$fname);
+		$mm->to=$eParams['to'];
+		$r=$mm->send();
+		unlink($this->tmpDir.$fname);
+		return $r;
+	}
+	private function sendToFtp($s,$sql)
+	{
+		$fParams=$s['ftp'];
+		
+		if(!($conn=ftp_connect($fParams['address']))) return false;
+		if(!ftp_login($conn,$fParams['username'],$fParams['password'])) return false;
+		
+		$tmpFile=$this->tmpDir.'phpMysqlBackup'.time().'.sql';
+		file_put_contents($tmpFile,$sql);
+		
+		$fname=$fParams['fileName'];
+		if(isset($fParams['timeSuffix']) && $fParams['timeSuffix']==true) $fname.=date('Y_m_d_H_i');
+		$fname.='.sql';
+		
+		$u=ftp_put($conn,$fParams['remoteDir'].$fname,$tmpFile,FTP_BINARY);
+		unlink($tmpFile);
+		return $u;
+	}
+}
+
+
+
+/*  bu belirtilen işleri çalıştırır. istediğin yerden bu iki satırı çağırabilirsin. fakat bu dosyanın içinden çağırmak yerine başka yerden çağır.
+örneğin bir kullanıcı giriş sayfan varsa, kullanıcı giriş yaptığı zaman çalıştırabilirsin. mantıklı olur.
+*/
+$x=new phpMysqlBackup();
+$x->makeSchedules();
+
+?>
